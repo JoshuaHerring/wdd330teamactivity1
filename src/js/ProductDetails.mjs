704edc84e369c7d999b4d96dae44fcbbe0e4b2fb
Lindsay Garner <gar21034@byui.edu>
2023-01-27 11:55:59 -0700

    random changes tesst

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,6 +1,7 @@
 import ProductData from './ProductData.mjs';
 import { setLocalStorage, getParam } from "./utils.mjs";
 
+
 export class ProductDetails {
     constructor(productId, dataSource) {
         this.productId = productId;
@@ -12,37 +13,45 @@ export class ProductDetails {
     async init(){
         const product = await this.dataSource.findProductById(this.productId);
         this.product = product;
-        console.log(product)
+        // console.log(product)
         const element = document.querySelector(".product-detail");
         element.innerHTML = this.renderProductDetails();
     }
 
     async addProductToCart(product) {
-        setLocalStorage("so-cart", product);
+        product = await product;
+        setLocalStorage("so-cart", this.product);
     }
 
     renderProductDetails(){
+        
         return `<h3>${this.product.Brand.Name}</h3>
-
+        
         <h2 class="divider">${this.product.NameWithoutBrand}</h2>
-
+        
         <img
-          class="divider"
-          src="${this.product.Image}"
-          alt="Marmot Ajax tent"
+        class="divider"
+        src="${this.product.Image}"
+        alt="Marmot Ajax tent"
         />
-
+        
         <p class="product-card__price"${this.product.FinalPrice}</p>
-
+        
         <p class="product__color">${this.product.Colors[0].ColorName}</p>
-
+        
         <p class="product__description">
         ${this.product.DescriptionHtmlSimple}
         </p>
-
         <div class="product-detail__add">
-          <button id="addToCart" data-id="880RR">Add to Cart</button>
+        <button> Add To Cart </button>
         </div>`
     }
     
+    makeButton(){
+        const button = document.createElement("button");
+        button.classList.add("addToCart"); 
+        
+    }
+    
 }
+
--- src/js/product.js
+++ src/js/product.js
@@ -2,15 +2,11 @@ import { setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import { ProductDetails } from "./ProductDetails.mjs";
 
-
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
-console.log(tent);
+// const showProduct = product.init();
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -18,6 +14,9 @@ async function addToCartHandler(e) {
 }
 
 // add listener to Add to Cart button
+// window.onload=function(){
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+// }
