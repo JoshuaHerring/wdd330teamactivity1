7e60227162dab595ff80b0e67cf61583f2720c4b
Lindsay Garner <gar21034@byui.edu>
2023-02-21 11:38:27 -0700

    checkout starts

--- src/js/cartTotal.js
+++ src/js/cartTotal.js
@@ -1,8 +1,9 @@
 export default function renderTotal(cartItems){ 
-    document.querySelector(".cart-total").innerHTML = `Total: $${getTotal(cartItems)}`;
+    let cartTotal = getTotal(cartItems)
+    document.querySelector(".cart-total").innerHTML = `Total: $${cartTotal}`;
 }
 
-function getTotal(cartItems){
+export function getTotal(cartItems){
     let total = 0;
     cartItems.forEach(item => {
     total += item.FinalPrice;
@@ -10,3 +11,20 @@ function getTotal(cartItems){
     return total;
 }
 
+
+export function getTaxTotal(cartTotal){
+    let taxTotal = cartTotal * 0.06;
+    return taxTotal;
+}
+
+
+// Use $10 for the first item plus $2 for each additional item for shipping.
+export function getShipping(cartItems){
+    let ship;
+    if(cartItems.length == 1){
+        ship = 10;
+    } else if(cartItems.length > 1){
+        ship = 10 + (2 * (cartItems.length - 1))
+    }
+    return ship;
+}
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,9 +1,14 @@
 import renderTotal from "./cartTotal";
 import { getLocalStorage } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProcess.mjs";
 
 const cartItems = getLocalStorage("so-cart");
 console.log(cartItems)
 
 document.querySelector(".itemSubtotal").innerHTML = `(${cartItems.length})`;
+
+const checkout = new CheckoutProcess;
+await checkout.init();
+console.log(checkout)
 
 renderTotal(cartItems)
