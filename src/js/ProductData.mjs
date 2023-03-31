6f27ff9a2d22ce651eb2f2dc2e27991815f1546f
Joseph-kaku <samkaks18@gmail.com>
2023-03-31 15:13:40 -0600

    little css added

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,4 @@
-import { convertToJson } from "./utils.mjs";
+import { setLocalStorage } from "./utils.mjs";
 
 const baseURL = "https://wdd330-backend.onrender.com/";
 
@@ -6,13 +6,13 @@ export default class ProductData {
   async getData(category) {
     const url = new URL(`${baseURL}products/search/${category}`);
     const response = await fetch(url);
-    const data = await convertToJson(response);
+    const data = await setLocalStorage(response);
     return data.Result;
   }
   async getProduct(id) {
     const url = new URL(`${baseURL}product/${id}`);
     const response = await fetch(url);
-    const data = await convertToJson(response);
+    const data = await setLocalStorage(response);
     return data.Result;
   }
   async findProductById(id) {
