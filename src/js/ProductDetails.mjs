e07ce581ea5fe57a5d96d868fce6d0339ee79d28
nfluckiger20 <flu20002@byui.edu>
2023-04-05 12:57:15 -0600

    discount working?

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -8,53 +8,146 @@ export class ProductDetails {
     this.dataSource = dataSource;
     this.init();
   }
-
-  async init() {
-    // console.log(this.dataSource);
-    const product = await this.dataSource.findProductById(this.productId);
-    this.product = product;
+  
+  async init(breadcrumb = true) {
+    this.product = await this.dataSource.findProductById(this.productId);
+    document.querySelector(`.product-detail`).innerHTML = this.renderProductDetails(this.product);
+    if (breadcrumb){
+      updateBreadCrumbs(
+        `
+        <a href="/product-listing">Home</a> / <a href="/product-listing/?category=${this.product.Category}">${this.product.Category}</a> / ${this.product.NameWithoutBrand}`
+      )
+    }
     console.log(this.product);
-    const element = document.querySelector(".product-detail");
-    element.innerHTML = await this.renderProductDetails();
+    document.querySelector(`.product-detail`).innerHTML =
+      this.renderProductDetails(this.product);
 
+    document.querySelector(`.product-card__carousel`).innerHTML =
+      this.renderCarousel(this.product);
+
+    document.querySelector(`.product-card__colors`).innerHTML =
+      this.renderColorSwitch(this.product);
+
+    // add listener to Add to Cart button
     document
       .getElementById("addToCart")
-      .addEventListener("click", this.addToCartHandler.bind(this));
+      .addEventListener("click", this.addToCart.bind(this));
+
+    // add listener for images
+    document.querySelectorAll("[data-src]").forEach((item) => {
+      item.addEventListener("click", (e) => {
+        e.preventDefault();
+        console.log("Hi");
+        // switch
+        this.handleImageClick(item.dataset.src);
+      });
+    });
+  }
+
+  handleImageClick(data) {
+    // get all source
+    const sourcesets = document.querySelectorAll(`source`);
+    // crete handler for current mainImg's data-src
+    // switch images
+    document.querySelector(`.product-card__mainImg`).dataset.src = data;
+    document.querySelector(`.product-card__mainImg`).src = data;
+    // set source for sources
+    sourcesets.forEach((item) => {
+      item.srcset = data;
+    });
   }
 
   // add to cart button event handler
-  async addToCartHandler(e) {
-    console.log(this.dataSource);
-    const product = await this.dataSource.findProductById(e.target.dataset.id);
-    this.addProductToCart(product);
+  addToCart(e) {
+    const colorValue = document.querySelector(`select`).value;
+    // Get from storage - this.products
+    this.products =
+      getLocalStorage(`so-cart`) === null ? [] : getLocalStorage(`so-cart`);
+    // Check if item is in this.products
+    if (
+      this.products.find(
+        (item) => item.Id === this.product.Id && item.Colors === colorValue
+      )
+    ) {
+      // If it is in this.products, add qty + 1
+      this.products.map((item) => {
+        if (item.Id === this.product.Id) {
+          item.Quantity += 1;
+        }
+        return item;
+      });
+    } else {
+      // else: add qty key
+      const prod = { ...this.product, Quantity: 1, Colors: colorValue };
+      this.products = [...this.products, prod];
+    }
+    // Set local storage
+    setLocalStorage(`so-cart`, this.products);
+
+    const badge = this.renderSuperscript(this.products);
+    const bagParent = document.querySelector(`.cart a`);
+
   }
 
-  async addProductToCart(product) {
-    product = await product;
-    setLocalStorage("so-cart", this.product);
-  }
+  renderProductDetails(product) {
+    const newItem = `
+      <h3>${product.Brand.Name}</h3>
 
-  async renderProductDetails() {
-    return `<h3>${this.product.Brand.Name}</h3>
-        
-        <h2 class="divider">${this.product.NameWithoutBrand}</h2>
-        
+      <h2 class="divider">${product.NameWithoutBrand}</h2>
+
+      <picture>
+        <source media="(min-width: 650px) and (max-width: 899px)" srcset="${product.Images.PrimaryLarge}">
+        <source media="(min-width: 900px)" srcset="${product.Images.PrimaryExtraLarge}">
         <img
-        class="divider"
-        src="${this.product.Images.PrimaryLarge}"
-        alt="Marmot Ajax tent"
+          class="divider product-card__mainImg"
+          src="${product.Images.PrimaryMedium}"
+          alt="${product.Name}"
+          data-src="${product.Images.PrimaryMedium}"
         />
-        
-        <p class="product-card__price">$${this.product.FinalPrice}</p>
-        
-        <p class="product__color">${this.product.Colors[0].ColorName}</p>
-        
-        <p class="product__description">
-        ${this.product.DescriptionHtmlSimple}
-        </p>
-        <div class="product-detail__add">
-        <button data-id="${this.product.Id}" id="addToCart"> Add To Cart </button>
-        </div>`;
+      </picture>
+
+      <div class="divider product-card__carousel">
+      </div>
+
+      <div class="divider product-card__colors"></div>
+
+      <h3 class="product-card__markup">$${this.product.SuggestedRetailPrice}</h3>
+      <h2 class="product-card__price">$${this.product.FinalPrice}</h2>
+
+      <p class="product__color">${product.Colors[0].ColorName}</p>
+
+      <p class="product__description">
+        ${product.DescriptionHtmlSimple}
+      </p>
+
+      <div class="product-detail__add">
+        <button id="addToCart" data-id=${product.Id}>Add to Cart</button>
+      </div>`;
+    return newItem;
+  }
+
+  renderCarousel(product) {
+    const images = product.Images.ExtraImages;
+    const section = document.createElement(`div`);
+
+    // First Img
+    const firstImg = document.createElement(`div`);
+    firstImg.setAttribute("class", "product-card__carousel-image");
+    firstImg.innerHTML = `
+      <img src="${product.Images.PrimaryLarge}" alt="${product.Name}" data-src="${product.Images.PrimaryExtraLarge}">
+      `;
+    section.appendChild(firstImg);
+
+    // Extra Images
+    images.forEach((item) => {
+      const imageDiv = document.createElement(`div`);
+      imageDiv.setAttribute("class", "product-card__carousel-image");
+      imageDiv.innerHTML = `
+      <img src="${item.Src}" alt="${item.Title}" data-src="${item.Src}">
+      `;
+      section.appendChild(imageDiv);
+    });
+    return section.innerHTML;
   }
 
   renderColorSwitch(product) {
@@ -71,11 +164,4 @@ export class ProductDetails {
     tempDiv.appendChild(select);
     return tempDiv.innerHTML;
   }
-
-
-  makeButton() {
-    const button = document.createElement("button");
-    button.classList.add("addToCart");
-  }
 }
-
--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -37,17 +37,76 @@ export default class ProductList {
       .then((data) => data);
   }
 
-  async productCardTemplate(product) {
-    return `<li class="product-card">
+  // searchinh for a product and displaying the results
+async searchProduct(key) {
+  const searchInstert = document.querySelector("#search-products");
+  searchInstert.innerHTML = "";
+  const list = await this.dataSource.getData(this.category);
+  const filteredList = this.filterProduct(list);
+  const products = filteredList.filter(item => item.Name.toLowerCase().includes(key.toLowerCase()));
+  const render = products.map(this.productCardTemplate);
+  searchInstert.insertAdjacentHTML("afterbegin", render.join(""));
+}
 
-    <a href="/product_listing/index.html?product=${product.Id}">
+  showModal(productId) {
+    const item = this.filteredList.filter(
+      (product) => product.Id === productId
+    );
+    this.mainElement.insertAdjacentHTML(
+      `beforebegin`,
+      this.productCardModal(item[0]),
+    );
+    document.querySelector(`.product-bg`).addEventListener(`click`, () => {
+      // alert("Test");
+      // TODO: add event listener for removing modal
+      document.querySelector(`.product-bg`).remove();
+    });
+  }
+
+  productCardTemplate(product) {
+    return `<li class="product-card">
+            <a href="../product_pages/index.html?product=${product.Id}">
+            <img
+              src="${product.Images.PrimaryLarge}"
+              alt="Image of ${product.Name} "
+            />
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">${product.Name}</h2>
+            <p class="product-card__markup">$${product.SuggestedRetailPrice}</p>
+            <h3 class="product-card__price">$${product.FinalPrice}</h3>
+            </a>
+            <button class="lookup-button" data-id="${product.Id}">Look Up</button>
+          </li>`;
+  }
+
+  productCardModal(product) {
+    return `
+    <div class="product-bg">
+    <div class="product-modal">
+    <div>
+    <h1>${product.Brand.Name}</h1>
+    <a href="../product_pages/index.html?product=${product.Id}">More details</a>
+    </div>
+    <div>
+    <picture>
+    <source media="(min-width: 650px) and (max-width: 899px)" srcset="${product.Images.PrimaryLarge}">
+    <source media="(min-width: 900px)" srcset="${product.Images.PrimaryExtraLarge}">
     <img
-      src="${product.Images.PrimaryMedium}"
-      alt="Image of ${product.Name}"
+    class="divider"
+    src="${product.Images.PrimaryMedium}"
+    alt="${product.Name}"
     />
-    <h3 class="card__brand">${product.Brand.Name}</h3>
-    <h2 class="card__name">${product.Name}</h2>
-    <p class="product-card__price">$${product.ListPrice}</p></a>
-  </li>`;
+    </picture>
+    <h3 class="product-card__markup">$${product.SuggestedRetailPrice}</h3>
+    <h2 class="product-card__price">$${product.FinalPrice}</h2>
+    
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    </div>
+    </div>
+    </div>`;
   }
 }
